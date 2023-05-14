90cfb2cc770fcc4c4be20dd5d6773c6ff1c07cb9
Igor Polyakov <ihor.polyakov@gmail.com>
2023-05-14 23:25:43 +0700

    main.sh improved

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 DIR=$(dirname "$0")
-cd $DIR || exit
-source "$(pipenv --venv -q)/bin/activate" && python src/main.py $@
+cd $DIR || exit 1
+ENV_PATH=$(pipenv --venv -q 2>/dev/null)
+if [[ "$ENV_PATH" == "" ]]; then
+    echo "Environment was not found" >/dev/stderr
+    exit 1;
+fi;
+source "$ENV_PATH/bin/activate"
+python src/main.py $@
